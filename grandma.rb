cf412c4b646df23c371a58635e298a9390fce609
Kelvin Vaughn Lee Jr <gtlp4l@gmail.com>
2019-12-16 17:43:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
-def speak_to_grandma(phrase)
-  if phrase==phrase.upercase
+def speak_to_grandma(arg ="")
+ if arg == ""
+ phrase==phrase.upercase
     puts "No, not since 19381"
   else
-  if phrase
+  if phrase 
     
