f7c1b4f8692028354342c2a95cd4c741eeffffc1
Kelvin Vaughn Lee Jr <gtlp4l@gmail.com>
2019-12-16 17:51:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(arg ="")
  if arg == ""
    puts "HUH?! SPEAK UP, SONNY!"
   elsif arg =="".upercase
-    puts "No, NOT SINCE1938"
+    puts "No, NOT SINCE 1938"
   else arg == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN"
 end
